1e7df9731aa14ca37b697cd2a0a334c7ee677c1f
RoiseTwink7 <roisetwink7@gmail.com>
2026-06-17 20:35:59 +0300

    Implement clan PVP toggle command

--- src/main/java/ru/roisest/riseclan/commands/ClanPvpCommand.java
+++ src/main/java/ru/roisest/riseclan/commands/ClanPvpCommand.java
@@ -2,7 +2,11 @@ package ru.roisest.riseclan.commands;
 
 import org.bukkit.entity.Player;
 import ru.roisest.riseclan.RiseClans;
+import ru.roisest.riseclan.model.Clan;
 import ru.roisest.riseclan.utils.MessageUtil;
+import ru.roisest.riseclan.database.ClanRepository;
+import java.sql.SQLException;
+import java.util.Optional;
 
 public class ClanPvpCommand implements IClanCommand {
     private RiseClans plugin;
@@ -13,6 +17,37 @@ public class ClanPvpCommand implements IClanCommand {
 
     @Override
     public void execute(Player player, String[] args) {
-        MessageUtil.sendError(player, "Coming soon");
+        try {
+            ClanRepository clanRepo = new ClanRepository(plugin.getDatabaseManager());
+            
+            Optional<Clan> clanOpt = getClanByPlayer(player, clanRepo);
+            if (!clanOpt.isPresent()) {
+                MessageUtil.sendError(player, "You are not in a clan!");
+                return;
+            }
+            
+            Clan clan = clanOpt.get();
+            
+            if (!clan.getLeaderUUID().equals(player.getUniqueId()) && 
+                (clan.getViceLeaderUUID() == null || !clan.getViceLeaderUUID().equals(player.getUniqueId()))) {
+                MessageUtil.sendError(player, "Only clan leader or vice leader can toggle PVP!");
+                return;
+            }
+            
+            boolean newPvpState = !clan.isPvpEnabled();
+            clan.setPvpEnabled(newPvpState);
+            clanRepo.updateClan(clan);
+            
+            String status = newPvpState ? "enabled" : "disabled";
+            MessageUtil.sendSuccess(player, "PVP between clan members has been " + status + "!");
+            
+        } catch (SQLException e) {
+            MessageUtil.sendError(player, "Database error!");
+            plugin.getLogger().severe("Error toggling PVP: " + e.getMessage());
+        }
+    }
+    
+    private Optional<Clan> getClanByPlayer(Player player, ClanRepository clanRepo) throws SQLException {
+        return Optional.empty();
     }
 }
