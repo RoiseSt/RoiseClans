21a95975d952f7c4ee6685c6f0774416c9a4fb6d
RoiseStt <roisesttwink44@gmail.com>
2026-06-18 04:02:37 +0300

    Fix: Implement proper PlaceholderAPI clan placeholders with database lookup

--- src/main/java/ru/roisest/riseclan/placeholders/ClanPlaceholder.java
+++ src/main/java/ru/roisest/riseclan/placeholders/ClanPlaceholder.java
@@ -4,6 +4,9 @@ import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.entity.Player;
 import ru.roisest.riseclan.RiseClans;
 import ru.roisest.riseclan.database.ClanRepository;
+import ru.roisest.riseclan.model.Clan;
+import ru.roisest.riseclan.utils.MessageUtil;
+import java.util.Optional;
 
 public class ClanPlaceholder extends PlaceholderExpansion {
     private RiseClans plugin;
@@ -40,22 +43,53 @@ public class ClanPlaceholder extends PlaceholderExpansion {
 
         try {
             ClanRepository repo = new ClanRepository(plugin.getDatabaseManager());
+            Optional<Clan> clanOpt = repo.getClanByMember(player.getUniqueId());
             
             switch (identifier) {
                 case "pvp_status":
-                    return "Unknown";
+                    if (clanOpt.isPresent()) {
+                        Clan clan = clanOpt.get();
+                        return clan.isPvpEnabled() ? "&aВключен" : "&cВыключен";
+                    }
+                    return "&7Нет";
+                    
                 case "clan_name":
-                    return "None";
+                    if (clanOpt.isPresent()) {
+                        return clanOpt.get().getName();
+                    }
+                    return "&7Нет";
+                    
                 case "clan_kills":
+                    if (clanOpt.isPresent()) {
+                        return String.valueOf(clanOpt.get().getKills());
+                    }
                     return "0";
+                    
                 case "clan_deaths":
+                    if (clanOpt.isPresent()) {
+                        return String.valueOf(clanOpt.get().getDeaths());
+                    }
                     return "0";
+                    
                 case "clan_level":
+                    if (clanOpt.isPresent()) {
+                        return String.valueOf(clanOpt.get().getLevel());
+                    }
                     return "0";
+                    
                 case "clan_name_colored":
-                    return "&7None";
+                    if (clanOpt.isPresent()) {
+                        Clan clan = clanOpt.get();
+                        return MessageUtil.translate(clan.getColor() + clan.getName());
+                    }
+                    return MessageUtil.translate("&7Нет");
+                    
                 case "clan_color":
+                    if (clanOpt.isPresent()) {
+                        return clanOpt.get().getColor();
+                    }
                     return "&7";
+                    
                 default:
                     return null;
             }
